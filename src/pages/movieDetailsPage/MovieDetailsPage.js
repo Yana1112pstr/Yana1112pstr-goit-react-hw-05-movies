4b3accd805d1913ade1c0df4e799baec70af5e67
Yana <yana1112pstr@gmail.com>
2022-07-10 21:05:20 +0300

    thi com

--- src/pages/movieDetailsPage/MovieDetailsPage.js
+++ src/pages/movieDetailsPage/MovieDetailsPage.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { fetchMoviesByID } from 'api/api';
-import MovieCard from 'components/movieCard/MovieCard';
-import ButtonGoBack from 'components/buttonGoBack/ButtonGoBack';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchMoviesByID } from "api/api";
+import { lazy, Suspense } from "react";
+
+const MovieCard = lazy(() => import("components/movieCard/MovieCard"));
+const ButtonGoBack = lazy(() => import("components/buttonGoBack/ButtonGoBack"));
 
 function MovieDetailsPage() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
 
-    const {movieId} = useParams();
-    const [movie, setMovie] = useState(null);
+  useEffect(() => {
+    fetchMoviesByID(movieId).then((data) => setMovie(data));
+  }, [movieId]);
 
-    useEffect(() => {
-        fetchMoviesByID(movieId).then(data => setMovie(data))
-    }, [movieId])
+  // console.log(movie);
 
-    // console.log(movie);
-
-    
-    return (
-        <div>
-            {/* <Link to="/">Go back</Link> */}
-            <ButtonGoBack/>
-            {movie && <MovieCard movie={movie} />}
-        </div>
-    );
-};
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ButtonGoBack />
+        {movie && <MovieCard movie={movie} />}
+      </Suspense>
+    </div>
+  );
+}
 
 export default MovieDetailsPage;
